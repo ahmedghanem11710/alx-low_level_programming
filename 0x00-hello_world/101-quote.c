7ba98ad45f705f5a1376bf1c46c3897d7bf8f8f5
ahmedghanem11710 <ahmedghanem11710@gamil.com>
2023-07-13 16:43:25 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - A c program that prints a line to the standard error
- * Retern: 1 (Success)
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	return (0);
 }
