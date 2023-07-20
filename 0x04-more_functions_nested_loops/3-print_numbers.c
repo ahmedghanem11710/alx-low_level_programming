ef9b65986b89c89016601a2e61be3ff1af103fe6
ahmedghanem11710 <ahmedghanem11710@gamil.com>
2023-07-20 13:52:31 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,13 +5,14 @@
 *
 * Return: Always 0.
 */
+
 void print_numbers(void)
 {
 	int c;
 
-	for (c = 48 ; c < 58; c++)
+	for (c = 0; c < 10; c++)
 	{
 		_putchar(c);
 	}
-	_putchar('\n');
+	_putchar("\n");
 }
